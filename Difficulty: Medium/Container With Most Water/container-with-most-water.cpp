e024a0f3524f2f7a7a343a4649dda8da4b527262
TanmayVyavahare <158759902+TanmayVyavahare@users.noreply.github.com>
2025-01-13 18:24:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Container With Most Water/container-with-most-water.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+
+  public:
+    int maxWater(vector<int> &arr) {
+        int n = arr.size();
+        int left = 0;
+        int right = n-1;
+        int res=0;
+        while(left<right){
+            int water = min(arr[left],arr[right])*(right-left);
+            res = max(res,water);
+            if(arr[left]<arr[right]) left++;
+            else right--;
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+
+        // Read first array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        int res = ob.maxWater(arr);
+
+        cout << res << endl << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
